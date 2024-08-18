40b4d60dac2dc2c4b9cc93d9167731d4b2cecb28
Lokesh-Reddy011 <cs23btech11011@iith.ac.in>
2024-08-18 11:43:23 +0530

    update

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import fetch from 'node-fetch';
 import cors from 'cors'
+import 'dotenv/config'
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT||5000;
 
 app.use(cors());
 app.use(bodyParser.json());
